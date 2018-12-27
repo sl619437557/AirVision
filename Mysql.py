efcc9af8c54a935c2cad95e8c59d41c084aeaafb
sunlei <sunl1996@126.com>
2018-12-27 21:50:36 +0800

    111

--- Mysql.py
+++ Mysql.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 # @app.route('/')
 # def index():
 #     return render_template('hello.html')
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:1205@127.0.0.1:3306/Air'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=Flask
-db=SQLAlchemy(app)
-class airport(db.Model):
-    __tablename__='airport'
-    AirportID=db.Column(db.Integer,primary_key=True)
-    Name=db.Column(db.Text)
-    City=db.Column(db.Text)
-    Country=db.Column(db.Text)
-    IATA=db.Column(db.Text)
-    ICAO=db.Column(db.Text)
-    Latitude=db.Column(db.Float)
-    Longitude=db.Column(db.Float)
-    Altitude=db.Column(db.Integer)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1205@127.0.0.1:3306/Air'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Flask
+db = SQLAlchemy(app)
 
-print(airport(Country='China'))
+
+class Airport(db.Model):
+    __tablename__ = 'airport'
+    AirportID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.Text)
+    City = db.Column(db.Text)
+    Country = db.Column(db.Text)
+    IATA = db.Column(db.Text)
+    ICAO = db.Column(db.Text)
+    Latitude = db.Column(db.Float)
+    Longitude = db.Column(db.Float)
+    Altitude = db.Column(db.Integer)
+
